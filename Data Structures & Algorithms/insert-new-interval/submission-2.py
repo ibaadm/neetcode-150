a8495393a6f5fd9c143ac2db901123660c101003
Ibaad Muhammad <ibaad.muhammad01@gmail.com>
2026-09-03 18:03:22 +0200

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        res = []
+        i = 0
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        if i == n:
+            res.append(newInterval)
+            return res
+
+        if intervals[i][0] > newInterval[1]:
+            res.append(newInterval)
+            res += intervals[i:]
+            return res
+
+        new_start = min(intervals[i][0], newInterval[0])
+        while i < n and intervals[i][0] <= newInterval[1]:
+            i += 1
+        
+        res.append([new_start, max(newInterval[1], intervals[i-1][1])])
+
+        if i != n:
+            res += intervals[i:]
+        
+        return res
+
+"""
+while the current end time is less than the new start time, keep adding current
+if we reach the end of the list, just add new and return
+otherwise, the new start is the new interval start time
+"""
